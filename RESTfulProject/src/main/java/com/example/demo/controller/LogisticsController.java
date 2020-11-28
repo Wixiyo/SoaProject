5bd03afd488bd71831ce28fb22549c664de5ed14
Wixiyo <49804956+Wixiyo@users.noreply.github.com>
2020-11-29 01:47:35 +0800

    物流查询api，返回string

--- RESTfulProject/src/main/java/com/example/demo/controller/LogisticsController.java
+++ RESTfulProject/src/main/java/com/example/demo/controller/LogisticsController.java
@@ -1,30 +1,26 @@
 package com.example.demo.controller;
 
-import com.example.demo.entity.Article;
+
 import com.example.demo.entity.Deal;
-import com.example.demo.entity.User;
-import com.example.demo.result.ExceptionMsg;
-import com.example.demo.result.ResponseData;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestTemplate;
+
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.URL;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+
 
 @Path("/logistics")
 public class LogisticsController {
+    @Autowired
+    RestTemplateBuilder restTemplateBuilder;
+
     @GET
     @Path("/getLogistics/{nu}/{com}")
     @Produces(MediaType.APPLICATION_JSON)
@@ -35,47 +31,12 @@ public class LogisticsController {
         String s = SearchLogistics(deal);
         return s;
     }
-
     private String SearchLogistics(Deal deal){
-        //String com="zhongtong";
-        //String nu="75414074668446";
-        String com = deal.getCom();
-        String nu = deal.getNu();
-        BufferedReader in = null;
-        try {
-            //获取访问地址URL,注意GET请求若URL中包含中文字符的话,在高版本TOMCAT中会认为是不合法字符,可改为POST方式
-            URL url = new URL("http://route.showapi.com/64-34?showapi_appid=451917&com="+com+"&nu="+nu+"&callBackUrl=xxx&phone=&outCode=&showapi_sign=f8ca729443174ba1b73952f4c45b677c");
-            //创建HttpURLConnection对象
-            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-            //请求方式,默认是GET
-            conn.setRequestMethod("GET");
-            //连接主机的超时时间（单位：毫秒）
-            conn.setConnectTimeout(30000);
-            //从主机读取数据的超时时间（单位：毫秒）
-            conn.setReadTimeout(30000);
-            //设置是否输出,默认是false
-            conn.setDoOutput(false);
-            //设置是否读入,默认是true
-            conn.setDoInput(true);
-            String result = "";
-            int code = conn.getResponseCode();
-            if(code == 200){
-                //定义BufferedReader输入流读取响应,getInputStream()会自动建立连接,无需手动调用connect()连接
-                in = new BufferedReader(new InputStreamReader(conn.getInputStream(),"UTF-8"));
-                String line;
-                while ((line = in.readLine()) != null) {
-                    result += line;
-                }
-                //关闭输入流
-                in.close();
-            }
-            System.out.println("返回结果："+result);
-            return result;
-        } catch (Exception e) {
-            e.printStackTrace();
-            return "-1";
-        }
+        RestTemplate client= restTemplateBuilder.build();
+        ResponseEntity<String> responseEntity = client.getForEntity("http://route.showapi.com/64-34?showapi_appid=451917&com={1}&nu={2}&callBackUrl=xxx&phone=&outCode=&showapi_sign=f8ca729443174ba1b73952f4c45b677c", String.class,deal.getCom(),deal.getNu());
+        return responseEntity.getBody();
     }
+
 
 
 }
