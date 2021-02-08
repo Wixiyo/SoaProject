e74f379faf502428da5afa2dca318c5a851e0a3f
Wixiyo <49804956+Wixiyo@users.noreply.github.com>
2021-02-09 02:46:10 +0800

    1

---
+++ UmberllaProject/umbrellaProj/src/main/java/demo/GreetingClient.java
@@ -0,0 +1,29 @@
+package demo;
+import java.net.*;
+import java.io.*;
+ 
+public class GreetingClient
+{
+   public static void main(String [] args)
+   {
+      String serverName = "localhost";
+      int port = 6066;
+      try
+      {
+         System.out.println("连接到主机：" + serverName + " ，端口号：" + port);
+         Socket client = new Socket(serverName, port);
+         System.out.println("远程主机地址：" + client.getRemoteSocketAddress());
+         OutputStream outToServer = client.getOutputStream();
+         DataOutputStream out = new DataOutputStream(outToServer);
+ 
+         out.writeUTF("Hello from " + client.getLocalSocketAddress());
+         InputStream inFromServer = client.getInputStream();
+         DataInputStream in = new DataInputStream(inFromServer);
+         System.out.println("服务器响应： " + in.readUTF());
+         client.close();
+      }catch(IOException e)
+      {
+         e.printStackTrace();
+      }
+   }
+}
